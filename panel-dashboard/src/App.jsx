e79e5ddeb7df9906e78b83d2226b04d8b99656e8
parastoopourazizi <napourazizi@gmail.com>
2025-02-25 21:30:19 +0330

    add card component

--- panel-dashboard/src/App.jsx
+++ panel-dashboard/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header/Header.jsx";
 import SideBar from "./sidebar/SideBar.jsx";
+import Card from "./cards/Cards.jsx";
 function App() {
   return (
     <>
       <Header />
       <SideBar />
+      <Card />
     </>
   );
 }
---
+++ panel-dashboard/src/cards/Cards.jsx
@@ -0,0 +1,5 @@
+import "./card.css";
+function Card() {
+  return <div className="card"> dashboard</div>;
+}
+export default Card;
